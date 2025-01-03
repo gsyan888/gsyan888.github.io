3e2b4cf6cb76aab12214bd8c9ea5d45a732b79f7
gsyan888 <gsyan888+github@gmail.com>
2025-01-04 03:33:37 +0800

    Update ruffle-config.js

--- js/ruffle-config.js
+++ js/ruffle-config.js
@@ -1,6 +1,9 @@
 //set options of RufflePlayer
 window.RufflePlayer = window.RufflePlayer || {};
 
+//right click or long press in touch mode tigger menu show
+window.RufflePlayer.config.contextMenu = true;
+
 //optons for loading fonts and set default fonts (Chinese fonts)
 window.RufflePlayer.config.fontSources = ["https://gsyan888.github.io/fonts/Iansui-Regular.ttf"];
 window.RufflePlayer.config.defaultFonts = {
