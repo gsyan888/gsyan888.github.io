6bea332cb6a8c40f48586eda48e5f055c5337f14
gsyan888 <gsyan888+github@gmail.com>
2025-01-04 02:10:51 +0800

    Update ruffle-config.js

--- js/ruffle-config.js
+++ js/ruffle-config.js
@@ -1,10 +1,8 @@
 //set options of RufflePlayer
-window.addEventListener("load", (event) => {
-	window.RufflePlayer = window.RufflePlayer || {};
+window.RufflePlayer = window.RufflePlayer || {};
 
-	//optons for loading fonts and set default fonts (Chinese fonts)
-	window.RufflePlayer.config.fontSources = ["https://gsyan888.github.io/fonts/Iansui-Regular.ttf"];
-	window.RufflePlayer.config.defaultFonts = {
-		sans: ["Iansui Regular", "Noto Sans TC Regular", "Noto Sans TC Light", "Noto Sans TC Thin"]
-	};
-});
+//optons for loading fonts and set default fonts (Chinese fonts)
+window.RufflePlayer.config.fontSources = ["https://gsyan888.github.io/fonts/Iansui-Regular.ttf"];
+window.RufflePlayer.config.defaultFonts = {
+	sans: ["Iansui Regular", "Noto Sans TC Regular", "Noto Sans TC Light", "Noto Sans TC Thin"]
+};
